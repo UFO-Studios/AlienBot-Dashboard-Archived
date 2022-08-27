97b9dff252e3d663d98c207204cc986cfb2d7fcb
Niceygy <nceygy01@gmail.com>
2022-08-27 09:49:35 +0100

    added something...?

--- backend/system-health/app.ts
+++ backend/system-health/app.ts
@@ -1,2 +1,12 @@
+import { resourceUsage } from "process";
 
-export{}
+var usage = require('usage');
+ 
+var pid = process.pid // ID for the node process. can be used for any app
+usage.lookup(pid, function(_err: any, _result: any) { //declears "err" (any errors) & "result" (the result)
+ 
+});
+const result = 
+console.log(_result)
+//export{}
+//gets how much mem/cpu node is using 
---
+++ test.ts
@@ -0,0 +1,2 @@
+console.log(resourceUsage)
+export{}
