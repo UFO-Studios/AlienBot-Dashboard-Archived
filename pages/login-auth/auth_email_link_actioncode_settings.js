842432ad209ae76bb0529c2c0f18c6e2ad5d6ad5
Niceygy <nceygy01@gmail.com>
2022-08-27 10:47:24 +0100

    added the pkg, logging of for now

--- pages/login-auth/auth_email_link_actioncode_settings.js
+++ pages/login-auth/auth_email_link_actioncode_settings.js
@@ -1,14 +1,14 @@
 const actionCodeSettings = {
     // URL you want to redirect back to. The domain (www.example.com) for this
     // URL must be in the authorized domains list in the Firebase Console.
-    url: 'https://www.example.com/finishSignUp?cartId=1234',
+    url: 'https://alienbot.thealiendoctor.com/auth/authed',
     // This must be true.
     handleCodeInApp: true,
     iOS: {
-      bundleId: 'com.example.ios'
+      bundleId: 'com.ufoStudios.ab-20'
     },
     android: {
-      packageName: 'com.example.android',
+      packageName: 'com.ufoStudios.ab-20',
       installApp: true,
       minimumVersion: '12'
     },
