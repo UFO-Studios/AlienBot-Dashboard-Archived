9167e2e484e467c8cb0913d55b47b8fb86064a02
Niceygy <nceygy01@gmail.com>
2022-09-03 10:35:17 +0100

    added commment

--- index.js
+++ index.js
@@ -13,4 +13,3 @@ new Chart("ram", {
   },
   options:{}
 });
-//how to we make them autoMagically settable? well its easy, we just change the data then.... idk ok... how? we need it to get the latest valu (i can do that), then push all the values back  ok what values? pm2 has ram/cpu usage metrics ohh, i see, but we arent using pm2 rn tho but ill import those later no i meant in penguin's server, but getting the values from there is easy too... if its whati i think it is but were not going to be using it for long i mean 3-4 months still long, we can change it later then ooooooooooooooooooooor use pm2 in penguin's server not really possible  : why? it needs manage sys perms.  oh damn, cant we put it in a npm script? nope, but can we jsut focus on putting the carts in the boxes? okay cool
